c630f61a5491b9da42327124473fb0b1d34c1454
David Chudzicki <dchudz@gmail.com>
2015-03-06 09:46:59 -0800

    some example code

---
+++ image_example.py
@@ -0,0 +1,15 @@
+# This shows how to generate an "image" in bokeh.
+# (Could be used to display map. Could also be used similarly to geom_tile in ggplot2)
+import numpy as np
+from bokeh.plotting import *
+
+N = 1000
+x = np.linspace(0, 10, N)
+y = np.linspace(0, 10, N)
+xx, yy = np.meshgrid(x, y)
+d = np.sin(xx)*np.cos(yy)
+
+output_file("image_example.html")
+p = figure(x_range=[0, 10], y_range=[0, 10])
+p.image(image=[d], x=[0], y=[0], dw=[10], dh=[10], palette="Spectral11")
+show(p)
---
+++ map_example.py
@@ -0,0 +1,29 @@
+# This seems really hacky, but I managed to get a png file I downloaded into a Bokeh plot
+
+from bokeh.plotting import *
+from PIL import Image
+import numpy as np
+
+
+
+# Convert a map to an array:
+# manipulate the array so the map looks right
+# tile image from: https://a.tiles.mapbox.com/v4/examples.map-i86l3621/0/0/0.png?access_token=pk.eyJ1IjoidHJpc3RlbiIsImEiOiJuZ2E5MG5BIn0.39lpfFC5Nxyqck1qbTNquQ
+img = Image.open("tile.png").convert('RGBA')
+
+# could use any other image too:
+#img = Image.open("/Users/david/Dropbox/Photos/20150301 - Tahoe/anthonys_camera/20150301_142558.jpg").convert('RGBA')
+
+arr = np.array(img)
+image_2d = np.empty(arr.shape[0:2], dtype=np.uint32)
+view = image_2d.view(dtype=np.uint8).reshape(arr.shape)
+for i in range(arr.shape[0]):
+    for j in range(arr.shape[1]):
+        for k in range(arr.shape[2]):
+            view[i, j, k] = arr[i,j,k]
+
+rotated_2d = np.rot90(np.transpose(image_2d))
+output_file("png_to_bokeh_image.html")
+p = figure(x_range=[0,10], y_range=[0,10])
+p.image_rgba(image=[rotated_2d], x=[0], y=[0], dw=[10], dh=[10])
+show(p)
